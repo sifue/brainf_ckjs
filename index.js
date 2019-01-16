55dfdac43ad2f121c242fa3eea9b99d25ccb3236
Soichiro-Yoshimura <yoshimura@soichiro.org>
2019-01-16 21:32:29 +0900

    Prettier formatted.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 const buffer = new ArrayBuffer(65536);
 const mem = new Int32Array(buffer);
 const code = process.argv[2]; // node index.js "code";
@@ -7,26 +7,26 @@ let ptr = 0;
 
 for (let i = 0; i < code.length; i++) {
   const c = code.charAt(i);
-  if (c === ">") {
+  if (c === '>') {
     ptr++;
-  } else if (c === "<") {
+  } else if (c === '<') {
     ptr--;
-  } else if (c === "+") {
+  } else if (c === '+') {
     mem[ptr]++;
-  } else if (c === "-") {
+  } else if (c === '-') {
     mem[ptr]--;
-  } else if (c === ".") {
+  } else if (c === '.') {
     process.stdout.write(String.fromCharCode(mem[ptr]));
-  } else if (c === ",") {
-    const input = require("fs").readFileSync("/dev/stdin", "utf8");
+  } else if (c === ',') {
+    const input = require('fs').readFileSync('/dev/stdin', 'utf8');
     mem[ptr] = input.charAt(0);
-  } else if (c === "[") {
+  } else if (c === '[') {
     if (mem[ptr] === 0) {
-      while (code.charAt(i) !== "]") i++;
+      while (code.charAt(i) !== ']') i++;
     }
-  } else if (c === "]") {
+  } else if (c === ']') {
     if (mem[ptr] !== 0) {
-      while (code.charAt(i) !== "[") i--;
+      while (code.charAt(i) !== '[') i--;
     }
   }
 }
